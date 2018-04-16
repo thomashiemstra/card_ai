c8e88bfa595a3471329fd8a66839f6f6aac64587
thomashiemstra <thomas-hiemstra@hotmail.com>
2018-04-16 22:27:29 +0200

    Adds abstract class for a card game

---
+++ games/AbstractGame.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractCardGame(ABC):
+
+    @abstractmethod
+    def reset(self):
+        # reset the game and return an observation of the game like observe()
+        pass
+
+    @abstractmethod
+    def play(self, action):
+        # play an action, must be an int or a numpy int array
+        pass
+
+    @abstractmethod
+    def observe(self):
+        # return the current state of the game (numpy array of ints), reward (int) and done (boolean)
+        # returns np.array(state), reward, done
+        pass
+
+    @abstractmethod
+    def check_legality_actions(self, actions):
+        # input: array of proposed scores per action
+        # put all the scores of illegal moves to zero
+        # example move 0 illegal [0.5, 0.3, 0.2] -> [0, 0.3, 0.2]
+        pass
